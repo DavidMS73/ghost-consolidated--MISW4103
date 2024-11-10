f65cb8925a3929e2cc28e19d260a8155250bd239
Daniel Corzo <d.corzos@uniandes.edu.co>
2024-11-10 18:43:53 -0500

    Implementadas E0013, E0014 y E0015

--- Pruebas E2E/Puppeteer-Cucumber/features/pages/members_page.js
+++ Pruebas E2E/Puppeteer-Cucumber/features/pages/members_page.js
@@ -3,9 +3,60 @@ class MembersPageObject {
     this.page = page;
   }
 
-  
+  async clickCreateMemberButton() {
+    await this.page.waitForSelector('a[href="#/members/new/"]');
+    await this.page.click('a[href="#/members/new/"]');
+    await new Promise((r) => setTimeout(r, 500));
+  }
 
-  
+  async fillEmail(email) {
+    await this.page.waitForSelector('#member-email');
+    await this.page.type('#member-email', email);
+    await new Promise((r) => setTimeout(r, 500));
+  }
+
+  async fillName(name) {
+    await this.page.waitForSelector('#member-name');
+    await this.page.type('#member-name', name);
+    await new Promise((r) => setTimeout(r, 500));
+  }
+
+  async clickSaveMemberButton() {
+    await this.page.waitForSelector('button[data-test-button="save"]');
+    await this.page.click('button[data-test-button="save"]');
+    await new Promise((r) => setTimeout(r, 500));
+  }
+
+  async goToMembersList() {
+    await this.page.waitForSelector('a[href="#/members/"]');
+    await this.page.click('a[href="#/members/"]');
+    await new Promise((r) => setTimeout(r, 500));
+  }
+
+  async checkMemberInList(emailToCheck) {
+    await this.page.waitForSelector("h3.gh-members-list-name");
+    const emails = await this.page.$$("h3.gh-members-list-name");
+    for (const email of emails) {
+      const emailText = await this.page.evaluate((htmlTag) => htmlTag.innerText, email);
+      if (emailText === emailToCheck) return true;
+    }
+
+    return false;
+  } 
+
+  async checkErrorMessageByMissingEmail() {
+    await this.page.waitForSelector("div.gh-cp-member-email-name > div.form-group.max-width.error > p");
+    const error = await this.page.$("div.gh-cp-member-email-name > div.form-group.max-width.error > p");
+    const errorText = await this.page.evaluate((htmlTag) => htmlTag.innerText, error);
+    return errorText === "Please enter an email.";
+  }
+
+  async checkAvatarInitials(first, last) {
+    await this.page.waitForSelector("span.gh-member-avatar-label");
+    const avatar = await this.page.$("span.gh-member-avatar-label");
+    const avatarText = await this.page.evaluate((htmlTag) => htmlTag.innerText, avatar);
+    return avatarText === first + last;
+  }
 }
 
 module.exports = MembersPageObject;
--- Pruebas E2E/Puppeteer-Cucumber/features/step_definitions/members_steps.js
+++ Pruebas E2E/Puppeteer-Cucumber/features/step_definitions/members_steps.js
@@ -3,52 +3,39 @@ const scope = require("../support/scope");
 
 // When
 
-When("I click on new post button", async () => {
-  await scope.pages.posts.createPost();
+When("I click on new member button", async () => {
+  await scope.pages.members.clickCreateMemberButton();
 });
 
-When("I fill post title with {string}", async (title) => {
-  await scope.pages.posts.fillTitle(title);
+When("I fill member email with {string}", async (email) => {
+  await scope.pages.members.fillEmail(email);
 });
 
-When("I fill post description with {string}", async (description) => {
-  await scope.pages.posts.fillDescription(description);
+When("I fill member name with {string}", async (name) => {
+  await scope.pages.members.fillName(name);
 });
 
-When("I click on publish post button", async () => {
-  await scope.pages.posts.clickPublishPost();
-});
-
-When("I program a post to be published later", async () => {
-  await scope.pages.posts.publishPostLater();
-});
-
-When("I program a post to be published right now", async () => {
-  await scope.pages.posts.publishPostNow();
-});
-
-When("I upload a feature image", async () => {
-  await scope.pages.posts.uploadFeatureImage("./assets/forest.jpg");
+When("I click on save member button", async () => {
+  await scope.pages.members.clickSaveMemberButton();
 });
 
 // Then
 
-Then("I deploy the collapse menu of posts", async () => {
-  await scope.pages.posts.deployCollapsePostsMenu();
+Then("I go to members list", async () => {
+  await scope.pages.members.goToMembersList();
 });
 
-Then("I go to scheduled posts", async () => {
-  // Write code here that turns the phrase above into concrete actions
-  await scope.pages.posts.goToScheduledPosts();
+Then("the member {string} should be in the list", async (email) => {
+  const result = await scope.pages.members.checkMemberInList(email);
+  console.assert(result, `The member ${email} is not in the list`);
 });
 
-Then("the post {string} should be in the list", async (title) => {
-  // Write code here that turns the phrase above into concrete actions
-  const result = await scope.pages.posts.checkPostInList(title);
-  console.assert(result, `The post ${title} is not in the list`);
+Then("I should see an error message due to a missing email field", async () => {
+  const result = await scope.pages.members.checkErrorMessageByMissingEmail();
+  console.assert(result, `The member email is not missing`);
 });
 
-Then("I go to published posts", async () => {
-  // Write code here that turns the phrase above into concrete actions
-  await scope.pages.posts.goToPublishedPosts();
+Then("I should see the initials {string} and {string} in the user avatar", async (first, last) => {
+  const result = await scope.pages.members.checkAvatarInitials(first, last);
+  console.assert(result, `The initials ${first} and ${last} are not in the user avatar`);
 });

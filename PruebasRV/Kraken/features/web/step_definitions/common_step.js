0a6c6b7c612b38cc5c4e4fd4a5ad0600242b8767
DavidMS73 <gd.martinez@uniandes.edu.co>
2024-11-17 15:59:57 -0500

    Kraken save images

--- PruebasRV/Kraken/features/web/step_definitions/common_step.js
+++ PruebasRV/Kraken/features/web/step_definitions/common_step.js
@@ -1,6 +1,24 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
+const fse = require("fs-extra");
 
 // When
+
+Given(
+  "I save a ss of version {kraken-string} and feature {kraken-string} and scenario {kraken-string} and step {kraken-string}",
+  async function (version, feature, scenario, step) {
+    const screenshotPath = `./output/screenshots/${version}/${feature}/${scenario}/`;
+    const screenshotName = `step_${step}.png`;
+    const fullPath = `${screenshotPath}${screenshotName}`;
+
+    //Validate if the folder exists
+    if (!fse.pathExistsSync(screenshotPath)) {
+      fse.ensureDirSync(screenshotPath);
+    }
+
+    //Screenshot
+    await this.driver.saveScreenshot(fullPath);
+  }
+);
 
 When("I click preview button", async function () {
   await this.commonPO.clickPreviewButton();

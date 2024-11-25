c31c10b5b7d9e4d624e5ce1dbe09dcb99242dc84
Germán David Martínez Solano <gd.martinez@uniandes.edu.co>
2024-11-24 23:03:05 -0500

    Update properties.js

--- DataValidation/Puppeteer-Cucumber/properties.js
+++ DataValidation/Puppeteer-Cucumber/properties.js
@@ -2,6 +2,6 @@ module.exports = {
   BASE_URL: "http://localhost:3001/ghost",
   EMAIL: "your-email",
   PASSWORD: "your-password",
-  USE_API: false,
+  USE_API: true,
   LOAD_PSEUDO_RANDOM_BEFORE_ALL: true,
 };

264ea03ca95f9c2389d7cee59a424689fa55fa3f
DavidMS73 <gd.martinez@uniandes.edu.co>
2024-11-25 19:41:33 -0500

    USE_API

--- Semana8/Pruebas E2E/properties.js
+++ Semana8/Pruebas E2E/properties.js
@@ -2,6 +2,6 @@ module.exports = {
   BASE_URL: "http://localhost:3001/ghost",
   EMAIL: "your-email",
   PASSWORD: "your-password",
-  USE_API: true,
+  USE_API: false,
   LOAD_PSEUDO_RANDOM_BEFORE_ALL: true,
 };
